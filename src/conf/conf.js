905f7d3b57df4edbb83be3bda30b19cd0c155595
gouravmakur <gouravmakur@gmail.com>
2024-10-29 00:47:45 +0530

    API Key edit

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -4,7 +4,8 @@ const conf = {
     appwriteDatabaseId : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteCollectionId : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    appwriteBucketId : String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+    appwriteBucketId : String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+    appwriteApiKey : String(import.meta.env.API_KEY),
 }
 
 export default conf;
