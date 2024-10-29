49f741c607f146ef069c1727f491e07d212d810a
gouravmakur <gouravmakur@gmail.com>
2024-10-29 16:00:15 +0530

    API Key Fix

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -4,7 +4,7 @@ const conf = {
     appwriteDatabaseId : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteBucketId : String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-    appwriteApiKey : String(import.meta.env.API_KEY),
+    appwriteApiKey : String(import.meta.env.VITE_API_KEY),
     rte_ApiKey : String(import.meta.env.VITE_RTE_API_KEY)
 }
 
